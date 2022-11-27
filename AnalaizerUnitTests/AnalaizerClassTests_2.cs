00fbbc60a0da9d24bb85696ebc3ced178d864ca5
MikeAtom <sonicgod98@gmail.com>
2022-11-27 22:56:11 +0200

    added

---
+++ AnalaizerUnitTests/AnalaizerClassTests_2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using AnalaizerClassLibrary;
+
+namespace AnalaizerUnitTests
+{
+    [TestClass()]
+    public class AnalaizerClassTests_2
+    {
+        public TestContext TestContext { get; set; }
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", @"C:\Users\Lenovo\Desktop\Lab_1\Calculator\AnalaizerUnitTests\TestData_1.xml", "Test", DataAccessMethod.Sequential)]
+        
+        [TestMethod()]
+        public void IsOperatorTests()
+        {
+            ///Arrange
+            string incomingData = Convert.ToString(TestContext.DataRow["incomingData"]);
+            bool expected = Convert.ToBoolean(TestContext.DataRow["expected"]);
+            ///Actual
+            bool actual = AnalaizerClass.IsOperator(incomingData);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
